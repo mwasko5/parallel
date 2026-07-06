e729c5c487cc2b12d065f2c390a24613a7c3d9e2
mwasko5 <mwasko@arizona.edu>
2026-07-05 22:34:11 -0700

    small changes for Vivado implementation

--- src/hardware_thread_unit/accelerator_core_top.sv
+++ src/hardware_thread_unit/accelerator_core_top.sv
@@ -3,36 +3,56 @@
 // This module intersects the thread_top and controller
 module accelerator_core_top (
     input logic CLK,
-    input logic RST_N
+    input logic RST_N,
+
+    input logic [31:0] INSTRUCTION,
+
+    output logic [31:0] EXT_WRITE_DATA,
+    output logic [31:0] SIMT_OUT_DATA
 );
 
+    logic [7:0] read_addr_a_s;
+    logic [7:0] read_addr_b_s;
+    logic [7:0] write_addr_s;
+
+    logic [1:0] op_sel_s;
+
+    logic write_en_s;
+    logic ext_write_sel_s;
+
+    logic [255:0] ext_write_data_s;
+    logic [255:0] simt_out_data_s;
+
     controller controller1 (
-        .INST_IN(),          // 32-bit instruction from CPU/Memory
+        .INST_IN(INSTRUCTION), // 32-bit instruction from CPU/Memory
     
         // Outputs mapped to your thread_top.sv inputs
-        .READ_ADDR_A(),
-        .READ_ADDR_B(),
-        .WRITE_ADDR(),
-        .OP_SEL(),
-        .WRITE_EN(),
-        .EXT_WRITE_SEL()
+        .READ_ADDR_A(read_addr_a_s),
+        .READ_ADDR_B(read_addr_b_s),
+        .WRITE_ADDR(write_addr_s),
+        .OP_SEL(op_sel_s),
+        .WRITE_EN(write_en_s),
+        .EXT_WRITE_SEL(ext_write_sel_s)
     );
 
     thread_top thread_top1 (
         .CLK(CLK),
         .RST_N(RST_N),
     
-        .READ_ADDR_A(),
-        .READ_ADDR_B(),
-        .WRITE_ADDR(),
-        .OP_SEL(),
+        .READ_ADDR_A(read_addr_a_s),
+        .READ_ADDR_B(read_addr_b_s),
+        .WRITE_ADDR(write_addr_s),
+        .OP_SEL(op_sel_s),
     
-        .WRITE_EN(),
+        .WRITE_EN(write_en_s),
     
-        .EXT_WRITE_SEL(), // 1 = Write External Data, 0 = Write SIMT Data
-        .EXT_WRITE_DATA(), 
+        .EXT_WRITE_SEL(ext_write_sel_s), // 1 = Write External Data, 0 = Write SIMT Data
+        .EXT_WRITE_DATA(ext_write_data_s), 
     
-        .SIMT_OUT_DATA() // Renamed for clarity
+        .SIMT_OUT_DATA(simt_out_data_s) // Renamed for clarity
     );
+
+    assign EXT_WRITE_DATA = ext_write_data_s[31:0]; 
+    assign SIMT_OUT_DATA  = simt_out_data_s[31:0];
 
 endmodule
